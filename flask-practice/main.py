878741ee92baa9bf8320fc57fd80216c88fdaab1
ulgoon <ulgoon89@gmail.com>
2022-04-11 14:38:56 +0900

    feat: flask - route with variables

--- flask-practice/main.py
+++ flask-practice/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 # Create instance of the Flask object
 # __name__: represents the name of the application package
@@ -11,7 +12,20 @@ app = Flask(__name__)
 def index():
     return "flask works!"
 
+# relay external data
+@app.route('/posts')
+def show_post_list():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    to_serve = response.json()
+    return jsonify(to_serve)
+
+# route with variables
+@app.route('/post/<string:post_name>')
+def show_post(post_name=None):
+    return "This is post about {}".format(post_name)
+
 # Do app.run() at __main__ entry point.
 # Protect users from accidentally invoking the script when they didn't intend to.
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+    # If you have trouble with running like "No module 'flask'", please run '$ pip install flask' once.  
